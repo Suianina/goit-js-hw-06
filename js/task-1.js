d13895ed7e739b8f35deb910506e1e7e61f3bca0
Zhanna Suianina <cezyby3@gmail.com>
2024-06-02 11:49:11 +0300

    save

--- js/task-1.js
+++ js/task-1.js
@@ -8,26 +8,23 @@ const customer = {
   orders: ["Burger", "Pizza", "Salad"],
   // Change code below this line
   getBalance() {
-    return balance;
+    return this.balance;
   },
   getDiscount() {
-    return discount;
+    return this.discount;
   },
   setDiscount(value) {
-    discount = value;
+    this.discount = value;
   },
   getOrders() {
-    return orders;
+    return this.orders;
   },
   addOrder(cost, order) {
-    balance -= cost - cost * discount;
-    orders.push(order);
+    this.balance -= cost - cost * this.discount;
+    this.orders.push(order);
   },
   // Change code above this line
 };
-
-
-
 
 customer.setDiscount(0.15);
 console.log(customer.getDiscount()); // 0.15
--- js/task-3.js
+++ js/task-3.js
@@ -1,5 +1,31 @@
 `use strict`;
 
+class StringBuilder {
+  #value;
+
+  constructor(initialValue) {
+    this.#value = initialValue;
+  }
+
+   getValue() {
+    return this.#value;
+  }
+
+   padEnd(str) {
+    this.#value += str;
+  }
+
+   padStart(str) {
+    this.#value = str + this.#value;
+  }
+
+  padBoth(str) {
+    this.padStart(str);
+    this.padEnd(str);
+  }
+};
+
+
 const builder = new StringBuilder(".");
 console.log(builder.getValue()); // "."
 builder.padStart("^");
